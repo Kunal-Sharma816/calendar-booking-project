142b9d48316528e284cb4f0ac5daeb2e25d6d8d6
Kunal-Sharma816 <22bcs058@iiitdwd.ac.in>
2025-05-21 16:10:07 +0530

    remove /api

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json()) 
 app.use(morgan('dev'))
 
-app.use('/api/bookings' , bookingRoutes);
+app.use('/bookings' , bookingRoutes);
 
 app.use(notFound);
 app.use(globalErrorHandler);
